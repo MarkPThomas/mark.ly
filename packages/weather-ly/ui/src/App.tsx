b6f34078088343cfaf0ae2d9f989da6764bbb1b5
Mark P Thomas <markums@gmail.com>
2022-07-21 10:13:50 -0600

    Implemented basic page routing, separated components into different pages

--- packages/weather-ly/ui/src/App.tsx
+++ packages/weather-ly/ui/src/App.tsx
@@ -1,8 +1,14 @@
 import { useState } from 'react';
-import { Forecast } from './components/Forecast/Forecast';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
+
+import { ForecastCarousel } from './components/Forecast/ForecastCarousel';
 import { ForecastHourly } from './components/ForecastHourly/ForecastHourly';
 import { ForecastGrid } from './components/ForecastGrid/ForecastGrid';
-import { Button } from './shared/components/Button'
 import { ForeCastSelector } from './components/ForecastSelector/ForecastSelector';
 
 import pointsData from '../../server/db/mongo/data/weather_ly.points.json';
@@ -11,7 +17,7 @@ import groupsData from '../../server/db/mongo/data/weather_ly.pointGroups.json';
 import { IGroupResponse, IPointResponse } from '../../server/api/model';
 
 
-export const App = (props) => {
+export const App = () => {
   const forecastTypes = {
     grid: 'grid',
     weekly: 'weekly',
@@ -27,18 +33,22 @@ export const App = (props) => {
 
   return (
     <>
-      <div>
-        <Button message="Click For Grid Data!" onClick={handleClick} cbArgs={forecastTypes.grid} />
-        <Button message="Click For Weekly Forecast!" onClick={handleClick} cbArgs={forecastTypes.weekly} />
-        <Button message="Click For Hourly Forecast!" onClick={handleClick} cbArgs={forecastTypes.hourly} />
-        <ForeCastSelector
-          points={pointsData as IPointResponse[]}
-          pointGroups={groupsData as IGroupResponse[]}
-        />
-        {forecastType === forecastTypes.grid && <ForecastGrid />}
-        {forecastType === forecastTypes.weekly && <Forecast />}
-        {forecastType === forecastTypes.hourly && <ForecastHourly />}
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <ForeCastSelector
+              points={pointsData as IPointResponse[]}
+              pointGroups={groupsData as IGroupResponse[]}
+            />
+          } />
+          <Route path="/weekly" element={<ForecastCarousel />} />
+          <Route path="/hourly" element={<ForecastHourly />} />
+          <Route path="/gridData" element={<ForecastGrid />} />
+          <Route path="/home">
+          </Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
     </>
   );
 }
