444ef1bcc16f9045f879226f213cc8f71a0b3278
Mark P Thomas <markums@gmail.com>
2023-06-25 15:01:24 -0500

    Initial files

---
+++ packages/app-allInOne/server/database/postgres/migrations/002-init-tables.ts
@@ -0,0 +1,13 @@
+export class InitTables002 {
+  public async up(client): Promise<void> {
+    await client.query(
+      ``
+    );
+  }
+
+  public async down(client): Promise<void> {
+    await client.query(
+      ``
+    );
+  }
+}
