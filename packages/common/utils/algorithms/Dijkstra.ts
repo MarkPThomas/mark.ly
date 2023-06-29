0c26ef9a81f520c5ca12d30661660ea48f884364
Mark P Thomas <markums@gmail.com>
2023-06-29 17:54:03 -0500

    Added Dijkstra

---
+++ packages/common/utils/algorithms/Dijkstra.ts
@@ -0,0 +1,84 @@
+// Source: https://levelup.gitconnected.com/finding-the-shortest-path-in-javascript-dijkstras-algorithm-8d16451eea34
+
+export const findShortestPath = (graph: IGraph, startNode: string, endNode: string) => {
+  // Initialize tracking objects
+  const distances: INodeDistance = {
+    ...graph[startNode],
+    endNode: Infinity
+  };
+
+  const parents: IParent = { endNode: null };
+  for (let child in graph[startNode]) {
+    parents[child] = startNode;
+  }
+
+  // Fill out tracking objects with shortest distances and associated parents
+  const visited: string[] = [];
+  let node = shortestDistanceNode(distances, visited);
+  while (node) {
+    const childDistances = graph[node];
+    for (let child in childDistances) {
+      if (child === startNode) {
+        continue;
+      } else {
+        const newdistance = distances[node] + childDistances[child];
+        if (!distances[child] || newdistance < distances[child]) {
+          distances[child] = newdistance;
+          parents[child] = node;
+        }
+      }
+    }
+
+    visited.push(node);
+    node = shortestDistanceNode(distances, visited);
+  }
+
+  // Trace from end to start to get shortest path
+  let shortestPath = [endNode];
+  let parent = parents[endNode];
+  while (parent) {
+    shortestPath.push(parent);
+    parent = parents[parent];
+  }
+  shortestPath.reverse();
+
+  return {
+    distance: distances[endNode],
+    path: shortestPath,
+  };
+}
+
+const shortestDistanceNode = (distances: INodeDistance, visited: string[]) => {
+  let shortest = null;
+
+  for (let node in distances) {
+    let currentIsShortest = shortest === null || distances[node] < distances[shortest];
+
+    if (currentIsShortest && !visited.includes(node)) {
+      shortest = node;
+    }
+  }
+
+  return shortest;
+};
+
+interface IParent {
+  [key: string]: string | null;
+}
+
+interface INodeDistance {
+  [key: string]: number;
+}
+
+export interface IGraph {
+  [key: string]: INodeDistance;
+}
+
+// let graph = {
+// 	start: { A: 5, B: 2 },
+// 	A: { start: 1, C: 4, D: 2 },
+// 	B: { A: 8, D: 7 },
+// 	C: { D: 6, finish: 3 },
+// 	D: { finish: 1 },
+// 	finish: {},
+// };
