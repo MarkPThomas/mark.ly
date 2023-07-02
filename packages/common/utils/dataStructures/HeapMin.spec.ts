97932c25c3a8672c9209b8fb78a95023dfda6609
Mark P Thomas <markums@gmail.com>
2023-07-02 11:40:21 -0500

    Finished testing Heap data structure

--- packages/common/utils/dataStructures/HeapMin.spec.ts
+++ packages/common/utils/dataStructures/HeapMin.spec.ts
@@ -1,5 +1,51 @@
-describe('description', () => {
-  it('should ', () => {
+import { MinHeap } from './HeapMin';
 
-  })
+describe('##MinHeap', () => {
+  describe('#build', () => {
+    it('should construct a Min Heap where root node is first value given', () => {
+      const minHeap = new MinHeap();
+      minHeap.build([1, 5, 2]);
+
+      expect(minHeap.size()).toEqual(3);
+      expect(minHeap.poll()).toEqual(1);
+    });
+
+    it('should construct a Min Heap where root node is last value given', () => {
+      const minHeap = new MinHeap();
+      minHeap.build([5, 2, 1]);
+
+      expect(minHeap.size()).toEqual(3);
+      expect(minHeap.poll()).toEqual(1);
+    });
+
+    it('should construct a Min Heap where root node is middle value given', () => {
+      const minHeap = new MinHeap();
+      minHeap.build([5, 1, 2]);
+
+      expect(minHeap.size()).toEqual(3);
+      expect(minHeap.poll()).toEqual(1);
+    });
+  });
+
+  describe('#insert', () => {
+    it('should insert a min node such that it ends up as the root', () => {
+      const minHeap = new MinHeap();
+
+      minHeap.build([1, 2, 3]);
+      expect(minHeap.poll()).toEqual(1);
+
+      minHeap.insert(-1);
+      expect(minHeap.poll()).toEqual(-1);
+    });
+
+    it('should heapify such that the node ends up in the appropriate position', () => {
+      const minHeap = new MinHeap();
+
+      minHeap.build([1, 3, 5]);
+
+      minHeap.insert(-1);
+
+      expect(minHeap.toArray()).toEqual([-1, 1, 5, 3])
+    });
+  });
 })
