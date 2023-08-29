c5fc00d691895d29b157d8a674378750296ea990
Mark P Thomas <markums@gmail.com>
2023-08-29 12:04:50 -0600

    [MPT-123] Added documentation

--- packages/GIS-ly/ui/src/model/GIS/GeoJSON/types.ts
+++ packages/GIS-ly/ui/src/model/GIS/GeoJSON/types.ts
@@ -1,3 +1,19 @@
+/**
+ * JSON representation of a Position in the form of:
+ *
+ * [longitude, latitude [, altitude] ]
+ *
+ * @export
+ * @type Position
+ */
 export type Position = [number, number] | [number, number, number];
 
+/**
+ * JSON representation of a {@link BoundingBox} in the form of:
+ *
+ * [westLongitude, southLatitude, eastLongitude, northLatitude]
+ *
+ * @export
+ * @type bboxJson
+ */
 export type bboxJson = [number, number, number, number];
