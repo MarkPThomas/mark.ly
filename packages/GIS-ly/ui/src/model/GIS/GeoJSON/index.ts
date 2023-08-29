884b3c7c8750c0a951c37a2b407d1bd5ce2da50c
Mark P Thomas <markums@gmail.com>
2023-08-29 15:25:32 -0600

    [MPT-123] Updated barrel file exports

--- packages/GIS-ly/ui/src/model/GIS/GeoJSON/index.ts
+++ packages/GIS-ly/ui/src/model/GIS/GeoJSON/index.ts
@@ -3,6 +3,7 @@ export * from './exceptions';
 export * from './types';
 
 export * from './BoundingBox';
+export * from './FeatureProperty';
 export * from './Feature';
 export * from './FeatureCollection';
 export * from './Geometries';
