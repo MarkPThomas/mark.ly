ef9b7e0d2ef177f6a47da5d46a1ecee2fae204c7
Mark P Thomas <markums@gmail.com>
2023-09-01 23:43:59 -0600

    [MPT-123] Moving files

--- packages/GIS-ly/ui/src/model/GIS/GeoJSON/Geometries/index.ts
+++
@@ -1,9 +0,0 @@
-export { GeometryType as Geometry } from './Geometry';
-export * from './GeometryBuilder';
-export * from './GeometryCollection';
-export * from './LineString';
-export * from './MultiLineString';
-export * from './MultiPoint';
-export * from './MultiPolygon';
-export * from './Point';
-export * from './Polygon';
