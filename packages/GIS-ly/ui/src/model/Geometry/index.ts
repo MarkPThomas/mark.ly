b3c22d706b7193b5df29f7603f073f5f8ae31c7e
Mark P Thomas <markums@gmail.com>
2023-10-01 21:12:06 -0600

    Added convenience barrel file

---
+++ packages/GIS-ly/ui/src/model/Geometry/index.ts
@@ -0,0 +1,24 @@
+export {
+  VertexNode,
+  IVertexNode,
+  SegmentNode,
+  ISegmentNode,
+  Polyline,
+  IPolyline,
+  IPolylineSize,
+  EvaluatorArgs,
+  Range,
+  Reference
+} from './PolyLine';
+
+export {
+  Segment,
+  ISegment,
+  ISegmentProperties
+} from './Segment';
+
+export {
+  Vertex,
+  IVertex,
+  IVertexProperties
+} from './Vertex';
