b9bcaddb823d37092584c0527247529d1a4d2073
Mark P Thomas <markums@gmail.com>
2023-12-12 12:16:56 -0700

    [MPT-249] Mostly finished testing Route stats

--- packages/GIS-ly/ui/src/model/GIS/Core/Route/Stats/RouteStats.ts
+++ packages/GIS-ly/ui/src/model/GIS/Core/Route/Stats/RouteStats.ts
@@ -1,8 +1,20 @@
-import { IPolylineStats, PolylineStats } from "../../../../Geometry/Polyline/Stats/PolylineStats";
+import { Polyline, Vertex, VertexNode } from "../../../../Geometry";
+import {
+  IPolylineStats,
+  IPolylineStatsCriteria,
+  PolylineStats
+} from "../../../../Geometry/Polyline/Stats/PolylineStats";
+import { BasicStats } from "../../../../Geometry/Stats";
+import { PolylineRoute } from "../PolylineRoute";
 import { RoutePoint } from "../RoutePoint";
 import { RouteSegment } from "../RouteSegment";
 import { HeightStats, IHeight } from "./HeightStats";
 import { ISlope, SlopeStats } from "./SlopeStats";
+
+export interface IRouteStatsCriteria extends IPolylineStatsCriteria {
+  isHeightConsidered?: (number: number) => boolean;
+  isSlopeConsidered?: (number: number) => boolean;
+}
 
 export interface IRouteStats extends IPolylineStats {
   height: IHeight;
@@ -13,25 +25,80 @@ export class RouteStats<
   TVertex extends RoutePoint = RoutePoint,
   TSegment extends RouteSegment = RouteSegment,
   TStats extends IRouteStats = IRouteStats
->
-  extends PolylineStats<TVertex, TSegment> {
+> extends PolylineStats<TVertex, TSegment> {
 
+  protected override _isStatConsidered: IRouteStatsCriteria;
   protected _heightStats: HeightStats;
   protected _slopeStats: SlopeStats;
 
-  override addStats() {
-    super.addStats();
-
-    this._heightStats = new HeightStats();
-    this._heightStats.fromTo(this._firstVertex, this._lastVertex);
-
-    this._slopeStats = new SlopeStats();
-    this._slopeStats.fromTo(this._firstVertex, this._lastVertex);
+  override get stats(): TStats {
+    return this.compileStats() as TStats;
   }
 
-  protected override compileStats(): TStats {
+  protected constructor(
+    polyline?: Polyline<TVertex, TSegment>,
+    isStatConsidered?: IRouteStatsCriteria,
+    firstVertex?: VertexNode<TVertex, TSegment>,
+    lastVertex?: VertexNode<TVertex, TSegment>
+  ) {
+    super(polyline, isStatConsidered, firstVertex, lastVertex);
+
+    this._isStatConsidered = {
+      ...this._isStatConsidered,
+      isHeightConsidered: isStatConsidered?.isHeightConsidered,
+      isSlopeConsidered: isStatConsidered?.isSlopeConsidered
+    }
+  }
+
+  static fromRoute<
+    TVertex extends RoutePoint = RoutePoint,
+    TSegment extends RouteSegment = RouteSegment
+  >(
+    polyline: PolylineRoute<TVertex, TSegment>,
+    isStatConsidered?: IRouteStatsCriteria
+  ): RouteStats<TVertex, TSegment, IRouteStats> {
+    return new RouteStats<TVertex, TSegment, IRouteStats>(polyline, isStatConsidered);
+  }
+
+  static fromRoutePoints<
+    TVertex extends RoutePoint = RoutePoint,
+    TSegment extends RouteSegment = RouteSegment
+  >(
+    firstPoint: VertexNode<TVertex, TSegment>,
+    lastPoint: VertexNode<TVertex, TSegment>,
+    isStatConsidered?: IRouteStatsCriteria
+  ): RouteStats<TVertex, TSegment, IRouteStats> {
+    return new RouteStats<TVertex, TSegment, IRouteStats>(null, isStatConsidered, firstPoint, lastPoint);
+  }
+
+  protected override initializeAndAddStats() {
+    super.initializeAndAddStats();
+
+    this._heightStats = new HeightStats(this._isStatConsidered.isHeightConsidered);
+    this.addStatsByState(this._heightStats as unknown as BasicStats<TVertex, TSegment>);
+
+    this._slopeStats = new SlopeStats(this._isStatConsidered.isSlopeConsidered);
+    this.addStatsByState(this._slopeStats as unknown as BasicStats<TVertex, TSegment>);
+  }
+
+  // protected override compileStats(): TStats {
+  //   return this.propertiesAreNotInitialized()
+  //     ? undefined
+  //     : {
+  //       ...super.compileStats(),
+  //       height: this._heightStats.serialize(),
+  //       slope: this._slopeStats.serialize()
+  //     } as TStats;
+  // }
+
+  protected override propertiesAreNotInitialized(): boolean {
+    return super.propertiesAreNotInitialized() || !this._heightStats || !this._slopeStats;
+  }
+
+  protected override serialize() {
+    const baseJson = super.serialize();
     return {
-      ...super.compileStats(),
+      ...baseJson,
       height: this._heightStats.serialize(),
       slope: this._slopeStats.serialize()
     } as TStats;
