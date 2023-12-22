71a9724a8306f629d697708d666d0fb4280cd0bf
Mark P Thomas <markums@gmail.com>
2023-12-22 11:32:22 -0700

    [MPT-249] Added exports

--- packages/GIS-ly/ui/src/model/Geometry/Stats/index.ts
+++ packages/GIS-ly/ui/src/model/Geometry/Stats/index.ts
@@ -2,4 +2,6 @@ export { INodeOfInterest } from "./INodeOfInterest";
 export { BasicStats } from "./BasicStats";
 export { MaxMin } from "./MaxMin";
 export { MaxMinStats } from "./MaxMinStats";
+export { Median } from "./Median";
+export { StandardDeviationStats } from "./StandardDeviationStats";
 export { SumMean as Sum } from "./SumMean";
