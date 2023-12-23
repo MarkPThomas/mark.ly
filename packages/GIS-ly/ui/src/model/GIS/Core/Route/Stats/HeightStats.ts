e05262a57275c5a836e32968a5bdd5805adfd599
Mark P Thomas <markums@gmail.com>
2023-12-23 12:55:27 -0700

    [MPT-249] Fixed bugs in net/max/min height. Added median & Std.

--- packages/GIS-ly/ui/src/model/GIS/Core/Route/Stats/HeightStats.ts
+++ packages/GIS-ly/ui/src/model/GIS/Core/Route/Stats/HeightStats.ts
@@ -5,7 +5,9 @@ import {
   INodeOfInterest,
   BasicStats,
   MaxMinStats,
-  Sum
+  Sum,
+  Median,
+  StandardDeviationStats
 } from "../../../../Geometry/Stats";
 
 import { RoutePoint } from "../RoutePoint";
@@ -17,6 +19,8 @@ export interface IHeight {
   loss: number;
   max: INodeOfInterest<RoutePoint, RouteSegment>;
   min: INodeOfInterest<RoutePoint, RouteSegment>;
+  mdn: INodeOfInterest<RoutePoint, RouteSegment>;
+  std: number;
 }
 
 export class HeightStats
@@ -25,14 +29,6 @@ export class HeightStats
 
   get net(): number {
     return this.getNetHeight();
-  }
-
-  private _maxMin: MaxMinStats<RoutePoint, RouteSegment>;
-  get max(): INodeOfInterest<RoutePoint, RouteSegment> {
-    return this._maxMin?.range.max;
-  }
-  get min(): INodeOfInterest<RoutePoint, RouteSegment> {
-    return this._maxMin?.range.min;
   }
 
   private _gain: Sum;
@@ -45,10 +41,22 @@ export class HeightStats
     return this._loss.value;
   }
 
-  protected override initializeProperties() {
-    this._gain = new Sum(this.isAscending);
-    this._loss = new Sum(this.isDescending);
-    this._maxMin = new MaxMinStats<RoutePoint, RouteSegment>(this.getPtElevation, false, this._isConsidered);
+  private _maxMin: MaxMinStats<RoutePoint, RouteSegment>;
+  get max(): INodeOfInterest<RoutePoint, RouteSegment> {
+    return this._maxMin?.range.max;
+  }
+  get min(): INodeOfInterest<RoutePoint, RouteSegment> {
+    return this._maxMin?.range.min;
+  }
+
+  private _median: Median<RoutePoint, RouteSegment>;
+  get mdn(): INodeOfInterest<RoutePoint, RouteSegment> {
+    return this._median.median;
+  }
+
+  private _standardDev: StandardDeviationStats<RoutePoint, RouteSegment>;
+  get std(): number {
+    return this._standardDev.sigma;
   }
 
   protected isAscending(number: number): boolean {
@@ -59,8 +67,24 @@ export class HeightStats
     return number < 0;
   }
 
-  protected getPtElevation(point: RoutePoint): number {
-    return point?.elevation;
+  protected getPtElevationOrAltitude(point: RoutePoint): number {
+    return point?.elevation ?? point?.alt ?? 0;
+  }
+
+  protected override initializeProperties() {
+    this._gain = new Sum(this.isAscending);
+    this._loss = new Sum(this.isDescending);
+    this._median = new Median<RoutePoint, RouteSegment>(this.getPtElevationOrAltitude, this._isConsidered);
+
+    const isSegmentProperty = false;
+    this._standardDev = new StandardDeviationStats<RoutePoint, RouteSegment>(
+      this.getPtElevationOrAltitude,
+      isSegmentProperty,
+      this._isConsidered);
+    this._maxMin = new MaxMinStats<RoutePoint, RouteSegment>(
+      this.getPtElevationOrAltitude,
+      isSegmentProperty,
+      this._isConsidered);
   }
 
   protected override addProperties(segment: SegmentNode<RoutePoint, RouteSegment>) {
@@ -72,6 +96,8 @@ export class HeightStats
     this._loss.add(segment.val.height);
 
     this._maxMin.add(segment);
+    this._median.add(segment);
+    this._standardDev.add(segment);
   }
 
   protected override removeProperties(segment: SegmentNode<RoutePoint, RouteSegment>) {
@@ -83,12 +109,18 @@ export class HeightStats
     this._loss.remove(segment.val.height);
 
     this._maxMin.remove(segment);
+    this._median.remove(segment);
+    this._standardDev.remove(segment);
   }
 
 
   protected getNetHeight(): number | undefined {
-    const startElevation = this._firstVertex?.val?.elevation ?? 0;
-    const endElevation = this._lastVertex?.val?.elevation ?? 0;
+    const startElevation = this._firstVertex?.val?.elevation ?? this._firstVertex?.val?.alt ?? 0;
+    const endElevation = this._lastVertex?.val?.elevation ?? this._lastVertex?.val?.alt ?? 0;
+
+    console.log('getNetHeight: ')
+    console.log('_firstVertex: ', this._firstVertex)
+    console.log('_lastVertex: ', this._lastVertex)
 
     return endElevation - startElevation;
   }
@@ -99,7 +131,9 @@ export class HeightStats
       gain: this.gain,
       loss: this.loss,
       max: this.max,
-      min: this.min
+      min: this.min,
+      mdn: this.mdn,
+      std: this.std
     }
   }
 }
