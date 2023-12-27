49e401de8e5d0df6e8934de004a81deb9c481fa7
Mark P Thomas <markums@gmail.com>
2023-12-27 10:06:06 -0700

    Notes for later dev

--- packages/common/utils/units/conversion/baseUnits/Units.ts
+++ packages/common/utils/units/conversion/baseUnits/Units.ts
@@ -2,17 +2,24 @@ export interface IUnits {
   length: eLength
   time: eTime
   angle: eAngle
+  // temperature: eTemp
 }
 
 export interface IUnitOverrides {
   length?: eLength
   time?: eTime
   angle?: eAngle
+  // temperature?: eTemp
 }
 
 export enum eLength {
+  // 1/2, 1/4, 1/8, 1/16, 1/32 inch? Can convert to rounded number or string with binary search.
+  // inches = 'inches',
   feet = 'feet',
+  // yards = 'yards',
   miles = 'miles',
+  // millimeters = 'millimeters',
+  // centimeters = 'centimeters',
   meters = 'meters',
   kilometers = 'kilometers'
 }
@@ -27,6 +34,13 @@ export enum eTime {
 
 export enum eAngle {
   radians = 'radians',
-  degrees = 'degrees',
+  degrees = 'degrees',  // decimal degrees
+  // degreesMMSS = 'degreesMMSS', // format to mm:ss.xx - only as string?
   percent = 'percent'
 }
+
+// export enum eTemp {
+//   fahrenheight = 'fahrenheight',
+//   celcius = 'celcius',
+//   kelvin = 'kelvin'
+// }
