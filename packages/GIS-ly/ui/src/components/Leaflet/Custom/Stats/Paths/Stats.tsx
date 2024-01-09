70c91998e54ea0d49809cb1271b8f2c306f6e7c9
Mark P Thomas <markums@gmail.com>
2024-01-08 17:56:19 -0700

    [MPT-312] Added stats scroll overflow

--- packages/GIS-ly/ui/src/components/Leaflet/Custom/Stats/Paths/Stats.tsx
+++ packages/GIS-ly/ui/src/components/Leaflet/Custom/Stats/Paths/Stats.tsx
@@ -1,8 +1,17 @@
+import styled from "styled-components";
+
 import { IPolylineSize } from "../../../../../model/Geometry";
 import { ITrackStats } from "../../../../../model/GIS/Core/Track/Stats";
 import { ToggleGroup } from "../../ToggleGroup";
 import { RouteStats } from "./RouteStats";
 import { TrackStats } from "./TrackStats";
+
+const FlowControl = styled.div`
+  //max-width: 25rem;
+  max-height: 30rem;
+  overflow-y: auto;
+  overflow-x: hidden;
+`;
 
 export interface IEditedStats extends ITrackStats {
   size: IPolylineSize
@@ -19,17 +28,19 @@ export function Stats({ stats }: StatsProps) {
     <div>
       <h2 className="stats">Stats</h2>
       <hr />
-      <ToggleGroup
-        value={'Route'}
-        level={level}
-        children={[<RouteStats key={Date()} stats={stats} level={level + 1} />]}
-      />
-      <hr />
-      <ToggleGroup
-        value={'Track'}
-        level={level}
-        children={[<TrackStats key={Date()} stats={stats} level={level + 1} />]}
-      />
+      <FlowControl>
+        <ToggleGroup
+          value={'Route'}
+          level={level}
+          children={[<RouteStats key={Date()} stats={stats} level={level + 1} />]}
+        />
+        <hr />
+        <ToggleGroup
+          value={'Track'}
+          level={level}
+          children={[<TrackStats key={Date()} stats={stats} level={level + 1} />]}
+        />
+      </FlowControl>
     </div>
   )
 }
