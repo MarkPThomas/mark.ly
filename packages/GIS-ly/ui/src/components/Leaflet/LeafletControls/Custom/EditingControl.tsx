788949235937bbd5c9882e95d5fd8462da2832f8
Mark P Thomas <markums@gmail.com>
2024-01-11 13:25:42 -0700

    [MPT-309] Added icons, fixd css positioning of center controls

---
+++ packages/GIS-ly/ui/src/components/Leaflet/LeafletControls/Custom/EditingControl.tsx
@@ -0,0 +1,12 @@
+
+export type ControlProps = {
+}
+
+export function EditingControl({ }: ControlProps) {
+  return (
+    <div className="leaflet-bar item edit-control">
+      <div className="editing-label">Editing</div>
+    </div>
+
+  )
+}
