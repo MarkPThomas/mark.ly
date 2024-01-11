4beefd2621e46ddb8b0083a0ff8b215bbb158904
Mark P Thomas <markums@gmail.com>
2024-01-11 13:32:56 -0700

    [MPT-309] Final tweaks to PolylineComparison stats

--- packages/GIS-ly/ui/src/components/Leaflet/Custom/Stats/Paths/PolylineStatsComparison.tsx
+++ packages/GIS-ly/ui/src/components/Leaflet/Custom/Stats/Paths/PolylineStatsComparison.tsx
@@ -12,30 +12,32 @@ export type PolylineStatsProps = {
 
 export function PolylineStatsComparison({ statsInitial, statsCurrent }: PolylineStatsProps) {
   return (
-    <table className="track-comparison">
-      <thead>
-        <tr>
-          <th align="center">Track</th>
-          <th align="center">Points</th>
-          <th align="center">Segments</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td align="left">Initial</td>
-          <td align="right">{statsInitial.size.vertices}</td>
-          <td align="right">{statsInitial.size.segments}</td>
-        </tr>
-        {
-          statsCurrent ?
-            <tr>
-              <td align="left">Current</td>
-              <td align="right">{statsCurrent.size.vertices}</td>
-              <td align="right">{statsCurrent.size.segments}</td>
-            </tr>
-            : null
-        }
-      </tbody>
-    </table>
+    <>
+      <table className="track-comparison">
+        <thead>
+          <tr>
+            <th align="center">Track</th>
+            <th align="center">Points</th>
+            <th align="center">Segments</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td align="left">Initial</td>
+            <td align="right">{statsInitial.size.vertices}</td>
+            <td align="right">{statsInitial.size.segments}</td>
+          </tr>
+          {
+            statsCurrent ?
+              <tr>
+                <td align="left">Current</td>
+                <td align="right">{statsCurrent.size.vertices}</td>
+                <td align="right">{statsCurrent.size.segments}</td>
+              </tr>
+              : null
+          }
+        </tbody>
+      </table>
+    </>
   )
 }
