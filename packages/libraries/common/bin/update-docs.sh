fbff8300f8e7c80bc1de05166e29067bd17e5859
Mark P Thomas <markums@gmail.com>
2024-02-28 13:10:17 -0700

    [MPT-379] Converted common to package

---
+++ packages/libraries/common/bin/update-docs.sh
@@ -0,0 +1,5 @@
+DIR=common
+
+rm -rfv  ../../../../../../../MarkPThomas.github.io/apis/libraries/$DIR/
+mkdir ../../../../../../../MarkPThomas.github.io/apis/libraries/$DIR
+cp -r docs/* ../../../../../../../MarkPThomas.github.io/apis/libraries/$DIR
